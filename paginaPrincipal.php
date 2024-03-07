bea485cb020fc28ee927174d849e0f77992c91a2
Ernesto Garcia Valenzuela <0322103721@ut-tijuana.edu.mx>
2024-03-06 21:14:50 -0800

    aaa

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Agentes de Valorant</title>
+  <title>Valorant Agents</title>
   <link rel="stylesheet" href="styles.css">
   <style>
   .agentes-lista {
